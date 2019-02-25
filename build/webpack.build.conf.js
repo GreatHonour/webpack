b1b686dfbe195a79029b0dbcf33a32a22fe3dfda
铭记。 <wr41296@163.com>
2019-02-25 15:07:08 +0800

    Update webpack.build.conf.js
    //只需要配置mode为"production"，即可显式激活 UglifyjsWebpackPlugin 插件

--- build/webpack.build.conf.js
+++ build/webpack.build.conf.js
@@ -6,19 +6,15 @@ const webpack = require('webpack');
 // 公用的配置
 const BASE_CONFIG = require('./webpack.base.conf')('production');
 
-// js代码压缩，速度相对较快，多线程
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-
 // 提取css 
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-// 压缩了代码、删掉了代码中无用的注释、还去除了冗余的 css、优化了 css 的书写顺序
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 
 // 打包视图可视化
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const BUILD_CONFIG = {
+    // 只需要配置mode为"production"，即可显式激活 UglifyjsWebpackPlugin 插件
     mode: 'production',                                  // 当前环境是开发还是生产
     devtool: false,
 
@@ -57,34 +53,6 @@ const BUILD_CONFIG = {
                 }
             }
         },
-
-        minimizer: [
-            // js代码压缩，速度相对较快，多线程
-            new UglifyJsPlugin({
-                cache: true,            // 开启缓存
-                parallel: true,
-                sourceMap: false,       // 正式不用开
-                uglifyOptions: {        
-                    ecma: 8,
-                    warnings: false,                // 显示警告
-                    compress: {
-                        warnings: false,            // 在删除没用到代码时 不输出警告
-                        drop_console: true,         // 删除console
-                        collapse_vars: true,        // 把定义一次的变量，直接使用，取消定义变量
-                        reduce_vars: true           // 合并多次用到的值，定义成变量
-                    },
-                    output: {
-                        beautify: false,            // 代码压缩成一行 true为不压缩 false压缩
-                        comments: true              // 去掉注释
-                    },
-                    toplevel: false,                // 启用顶级变量和函数名称修改并删除未使用的变量和函数
-                    ie8: false,                     // 启用IE8支持
-                }
-            }),
-
-            // css 压缩
-            new OptimizeCSSAssetsPlugin({})
-        ]
     },
 
     plugins: [
